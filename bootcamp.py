078b66eb1400a01f71e64ee814a419f9c0dcc581
Rafael Wanderley <Rafaelwvs16@icloud.com>
2022-09-28 11:44:35 -0300

    testes de variáveis

--- bootcamp.py
+++ bootcamp.py
@@ -12,3 +12,11 @@ bool()
 dir()
 dir(100)
 dir()
+
+age = 31
+name = 'Rafael'
+profissão = 'Programador e Chefe de cozinha'
+hobby = 'trilhas, treinar e estudar'
+print(age)
+
+print(f'Meu nome é {name} e eu Tenho {age} ano(s) de idade, eu trabalho como {profissão}, gosto de {hobby} no meu tempo livre')
