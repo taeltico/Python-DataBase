28f84ae2129ac939dcc165810a8d0b0dfe762f2d
Rafael Wanderley <Rafaelwvs16@icloud.com>
2022-09-28 12:15:21 -0300

    convertendo variaveis

---
+++ convertendoVariaveis.py
@@ -0,0 +1,23 @@
+idade = 31
+preço = 150,99
+
+print(idade)
+
+print(idade/2)
+
+print(str(idade))
+
+texto = f"idade {idade} preço {preço}"
+print(texto)
+
+print(int(2.3468465))
+
+print(float(30.433))
+
+valor = 342,4
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(243/3)
+print(342//2)
