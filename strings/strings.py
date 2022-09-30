d33b3099aef4c129620a7c0123c7f9943a2f9114
Rafael Wanderley <Rafaelwvs16@icloud.com>
2022-09-30 19:35:23 -0300

    strings 2

--- strings/strings.py
+++ strings/strings.py
@@ -1,8 +1,18 @@
-from tkinter import _Cursor
-
-
 curso = "Python"
 
-print(curso.enter(10, "#"))
+print(curso.center(10, "#"))
+print(".".join(curso))
 
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+print(curso.upper())
+
+text = "  Hello World!  "
+
+print(text.strip()+ "&")
+print(text.rstrip()+ "&")
+print(text.lstrip()+ "&")
+
+print(text.center(20, "$"))
 print(".".join(curso))
