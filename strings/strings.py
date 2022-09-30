1237f247474c74ea4785a530426eb99d1a00b0fa
Rafael Wanderley <Rafaelwvs16@icloud.com>
2022-09-30 20:01:18 -0300

    interpolação

--- strings/strings.py
+++ strings/strings.py
@@ -16,3 +16,19 @@ print(text.lstrip()+ "&")
 
 print(text.center(20, "$"))
 print(".".join(curso))
+
+for letra in text:
+    print(letra)
+
+nome = "Rafael"
+idade = 31
+profissão = "Pizzaiolo"
+idiomas = "Inglês, Espanhol, Português"
+
+print("Olá, Me chamo %s. eu tenho %d  anos de idade, trabalho como %s e estou aprendendo %s." % (nome, idade, profissão, idiomas))
+
+print("Olá, Me chamo {}. eu tenho {}  anos de idade, trabalho como {} e estou aprendendo {}." .format(nome, idade, profissão, idiomas))
+
+print("Olá, Me chamo {3}. eu tenho {2}  anos de idade, trabalho como {1} e estou aprendendo {0}." .format( idiomas, profissão, idade, nome))
+
+print("Olá, Me chamo {nome}. eu tenho {idade} anos de idade, trabalho como {profissão} e estou aprendendo {idioma}." .format(nome=nome, idade=idade, profissão=profissão, idiomas=idiomas))
