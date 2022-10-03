181e739239ad8e7ded380f23d4c260f0137a8a99
˜Rafael <rafaelwvs16@gmail.com>
2022-10-03 16:26:02 -0300

    corrigindo erros

--- pandas/appBanco.py
+++ pandas/appBanco.py
@@ -1,15 +1,11 @@
-from logging.handlers import RotatingFileHandler
-
-
 nome = input("digite eu nome ")
 
 saldo = 1000
 Opções = int(input("Informe uma opção:[1]Saque\n [2] Extrato \n [3] Investimentos\n [4]Depositar:"))
 saque = (saldo - ("Informe o valor que deseja sacar ")) 
 
-
 if Opções == 1:
-    valor = float(input("Informe o valor que deseja sacar "))
+    valor = input("Informe o valor que deseja sacar ")
     
     if saque<=saldo:
         print("saque autorizado", (nome), "Seu saldo disponivel e ", (saldo-saque))
@@ -23,13 +19,5 @@ elif Opções == 3:
     print("Abrindo investimentos")
 
 elif Opções == 4:
-    deposito = input("digite o valor de deposito") + saldo
+    deposito = float(input("digite o valor de deposito")+ saldo)
     print(deposito)
-
-
-
-
-
-
-
-
