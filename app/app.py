3b61da0ea0e514232ad481a084747faace565354
Rafael <rafaelwvs16@icloud.com>
2022-10-11 22:49:39 -0300

    inicio do app de banco

---
+++ app/app.py
@@ -0,0 +1,10 @@
+import email
+
+
+nome = input("digite seu nome ")
+mail = input("digite seu e-mail ")
+telefone = input("digite seu telefone ")
+endereco = input("digite seu endereço ")
+
+print("Olá meu nome é", (nome), "meu numero de telefone é ", (telefone), "meu e-mail é ", (mail), "moro no endereço ", (endereco))
+
--- strings/desafio.py
+++ strings/desafio.py
@@ -5,9 +5,5 @@ diametro1 = int(entrada[1])
 diametro2 = int(entrada[2])
 resultado = distancia/(diametro1+diametro2)
 
-if (0 < distancia < 10000):
-  if(0< diametro1,diametro2< 100):
-    print(f'{resultado:.2f}')
-
 # TODO: Calcule o ICM da comunicação dos Palatír de Sauron e Saruman, com 2 casas decimais no espaço #em branco Abaixo
 ICM = ((diametro2+diametro1)/distancia)
