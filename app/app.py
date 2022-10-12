96108a0f44122da4fc1cefee7431528c6eb43694
Rafael <rafaelwvs16@icloud.com>
2022-10-11 22:57:34 -0300

    app 2

--- app/app.py
+++ app/app.py
@@ -3,8 +3,14 @@ import email
 
 nome = input("digite seu nome ")
 mail = input("digite seu e-mail ")
-telefone = input("digite seu telefone ")
+telefone =int(input("digite seu telefone "))
 endereco = input("digite seu endereço ")
 
 print("Olá meu nome é", (nome), "meu numero de telefone é ", (telefone), "meu e-mail é ", (mail), "moro no endereço ", (endereco))
 
+
+Saldo = 50223
+saque = input(" digite o valor ")
+valorDisponivel = (Saldo-saque)
+
+print(nome , "seu saldo disponivel é",valorDisponivel)
