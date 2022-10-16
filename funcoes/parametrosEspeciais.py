5901d101d3137c84f349471201724a872795af24
Rafael <Rafaelwvs16@gmail.com>
2022-10-16 10:32:00 -0300

    funcoes 2

--- funcoes/parametrosEspeciais.py
+++ funcoes/parametrosEspeciais.py
@@ -1,4 +1,26 @@
-def f(pos1, pos2, /, pos_or_kwd, *, kwd1, kwd2):
-    print(pos1, pos2, pos_or_kwd, kwd1, kwd2)
 
-print(1, 2, 3, kwd1 = 23, kwd2 = 89)
+def somar(a, b):
+    return a + b
+
+def multi(a, b):
+    return a * b
+
+def exibir_retorno(a, b, funcao):
+    resultado = funcao(a, b)
+    print(f"o resultado da opracao {a} + {b} = {resultado}")
+    return(f"o resultado da opracao {a} + {b} = {resultado}")
+
+def tentativa(multi, exibir_retorno):
+    total = multi(exibir_retorno)
+    return(f"total é {multi} * {exibir_retorno} = {total}")
+
+
+print(exibir_retorno(26,15,somar))
+print(exibir_retorno(26,15, multi))
+print(tentativa(multi,exibir_retorno,tentativa))
+
+
+
+
+
+
