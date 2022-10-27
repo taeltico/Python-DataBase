c0f6a5ab88dfbc7845efa8e5d95bdded4bf85660
Rafael <Rafaelwvs16@gmail.com>
2022-10-27 19:31:24 -0300

    correcao

--- desafios/bicicletaria.py
+++ desafios/bicicletaria.py
@@ -1,10 +1,9 @@
 class Bicicleta ():
     def __init__(self, cor, modelo, ano, valor):
-    
-    self.ano = ano
-    self.cor = cor
-    self.valor = valor
-    self.modelo = modelo
+        self.ano = ano
+        self.cor = cor
+        self.valor = valor
+        self.modelo = modelo
 
     def buzinar(self):
         print("plim plim....")
@@ -17,3 +16,4 @@ class Bicicleta ():
         print("VRUMMNM...")
 
 b1 = Bicicleta("vernelho", "caloi", 2022, 600)
+b2 = Bicicleta("azul", "Shimano", 2010, 1.200)
